85165d620b629e8b34f3d0e80d4fcb8c028b65d0
gdiguglielmo <gusbertus@gmail.com>
2024-06-03 13:45:56 +0200

    update src folder

--- src/scripts/general/index.js
+++ src/scripts/general/index.js
@@ -1,5 +1,24 @@
-function showConsole() {
-  console.log("test");
-}
+// Get all the elements you want to show on scroll
+const targets = document.querySelectorAll(".js-show-on-scroll");
 
-showConsole();
+// Callback for IntersectionObserver
+const callback = function(entries) {
+    entries.forEach(entry => {
+        const animationType = entry.target.dataset.animateType;
+        if (entry.isIntersecting) {
+        //   entry.target.classList.add(animationType);
+      }
+    });
+  };
+
+// Set up a new observer
+const observer = new IntersectionObserver(callback);
+
+// Loop through each of the target
+targets.forEach(function(target) {
+  // Hide the element
+//   target.classList.add("opacity-0");
+
+  // Add the element to the watcher
+  observer.observe(target);
+});
